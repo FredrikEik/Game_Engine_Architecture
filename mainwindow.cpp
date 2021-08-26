296425f63e77bc341c436661ba06d112fa11c9e6
Ole Flaten <ole.flaten@inn.no>
2021-08-26 23:14:32 +0200

    Initial code.

---
+++ mainwindow.cpp
@@ -0,0 +1,88 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QSurfaceFormat>
+#include <QDebug>
+#include <QScreen>  //for resizing the program at start
+
+#include "renderwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    //this sets up what's in the mainwindow.ui
+    ui->setupUi(this);
+    init();
+}
+
+MainWindow::~MainWindow()
+{
+    delete mRenderWindow;
+    delete ui;
+}
+
+void MainWindow::init()
+{
+    //This will contain the setup of the OpenGL surface we will render into
+    QSurfaceFormat format;
+
+    //OpenGL v 4.1 - (Ole Flatens Mac does not support higher than this - sorry!)
+    //you can try other versions, but then have to update RenderWindow and Shader
+    //to inherit from other than QOpenGLFunctions_4_1_Core
+    format.setVersion(4, 1);
+    //Using the main profile for OpenGL - no legacy code permitted
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    //A QSurface can be other types than OpenGL
+    format.setRenderableType(QSurfaceFormat::OpenGL);
+
+    //This should activate OpenGL debug Context used in RenderWindow::startOpenGLDebugger().
+    //This line (and the startOpenGLDebugger() and checkForGLerrors() in RenderWindow class)
+    //can be deleted, but it is nice to have some OpenGL debug info!
+    format.setOption(QSurfaceFormat::DebugContext);
+
+    // The renderer will need a depth buffer - (not requiered to set in glfw-tutorials)
+    format.setDepthBufferSize(24);
+
+    //Set the number of samples used for multisampling
+    //Same as glfwWindowHint(GLFW_SAMPLES, 4);
+    //https://learnopengl.com/Advanced-OpenGL/Anti-Aliasing
+    format.setSamples(4);
+
+    //Just prints out what OpenGL format we try to get
+    // - this can be deleted
+    qDebug() << "Requesting surface format: " << format;
+
+    //We have a format for the OpenGL window, so let's make it:
+    mRenderWindow = new RenderWindow(format, this);
+
+    //Check if renderwindow did initialize, else prints error and quit
+    if (!mRenderWindow->context()) {
+        qDebug() << "Failed to create context. Can not continue. Quits application!";
+        delete mRenderWindow;
+        return;
+    }
+
+    //The OpenGL RenderWindow got made, so continuing the setup:
+    //We put the RenderWindow inside a QWidget so we can put in into a
+    //layout that is made in the .ui-file
+    mRenderWindowContainer = QWidget::createWindowContainer(mRenderWindow);
+    //OpenGLLayout is made in the .ui-file!
+    ui->OpenGLLayout->addWidget(mRenderWindowContainer);
+
+    //Set the size of the program in % of the actual screen size
+    QSize tempSize = QGuiApplication::primaryScreen()->size();
+
+    tempSize.rheight() *= 0.85;
+    tempSize.rwidth() *= 0.65;
+    resize(tempSize);
+
+    //sets the keyboard input focus to the RenderWindow when program starts
+    // - can be deleted, but then you have to click inside the renderwindow to get the focus
+    mRenderWindowContainer->setFocus();
+}
+
+//Example of a slot called from the button on the top of the program.
+void MainWindow::on_pushButton_toggled(bool checked)
+{
+    mRenderWindow->toggleWireframe(checked);
+}
---
+++ renderwindow.h
@@ -0,0 +1,98 @@
+#ifndef RENDERWINDOW_H
+#define RENDERWINDOW_H
+
+#include <QWindow>
+#include <QOpenGLFunctions_4_1_Core>
+#include <QTimer>
+#include <QElapsedTimer>
+
+#include "input.h"
+#include "constants.h"
+
+class QOpenGLContext;
+class Shader;
+class MainWindow;
+class VisualObject;
+class Camera;
+class Texture;
+
+// This inherits from QWindow to get access to the Qt functionality and
+// OpenGL surface.
+// We also inherit from QOpenGLFunctions, to get access to the OpenGL functions
+// This is the same as using glad and glw from general OpenGL tutorials
+class RenderWindow : public QWindow, protected QOpenGLFunctions_4_1_Core
+{
+    Q_OBJECT
+public:
+    RenderWindow(const QSurfaceFormat &format, MainWindow *mainWindow);
+    ~RenderWindow() override;
+
+    QOpenGLContext *context() { return mContext; }
+
+    void exposeEvent(QExposeEvent *) override;
+
+    void toggleWireframe(bool buttonState);
+
+private slots:
+    void render();
+
+private:
+    void init();
+
+    void checkForGLerrors();
+
+    void calculateFramerate();
+
+    void startOpenGLDebugger();
+
+    void setCameraSpeed(float value);
+
+    void handleInput();
+
+    void setupPlainShader(int shaderIndex);
+    GLint mMatrixUniform{-1};
+    GLint vMatrixUniform{-1};
+    GLint pMatrixUniform{-1};
+
+    void setupTextureShader(int shaderIndex);
+    GLint mMatrixUniform1{-1};
+    GLint vMatrixUniform1{-1};
+    GLint pMatrixUniform1{-1};
+    GLint mTextureUniform{-1};
+
+    class Texture *mTextures[gsl::NumberOfTextures]{nullptr}; //We can hold some textures
+
+    class Shader *mShaderPrograms[gsl::NumberOfShaders]{nullptr};    //holds pointer the GLSL shader programs
+
+    Camera *mCurrentCamera{nullptr};
+    float mAspectratio{1.f};
+
+    std::vector<VisualObject*> mVisualObjects;
+
+    Input mInput;
+    float mCameraSpeed{0.05f};
+    float mCameraRotateSpeed{0.1f};
+    int mMouseXlast{0};
+    int mMouseYlast{0};
+
+    QOpenGLContext *mContext{nullptr};
+    bool mInitialized;
+
+    QTimer *mRenderTimer{nullptr};           //timer that drives the gameloop
+    QElapsedTimer mTimeStart;       //time variable that reads the calculated FPS
+
+    MainWindow *mMainWindow{nullptr};        //points back to MainWindow to be able to put info in StatusBar
+
+    class QOpenGLDebugLogger *mOpenGLDebugLogger{nullptr};
+
+protected:
+    //The QWindow that we inherit from has these functions to capture mouse and keyboard.
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void keyPressEvent(QKeyEvent *event) override;
+    void keyReleaseEvent(QKeyEvent *event) override;
+    void wheelEvent(QWheelEvent *event) override;
+};
+
+#endif // RENDERWINDOW_H
