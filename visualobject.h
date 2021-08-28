c69a6eafc06ee8be2ec9d404615cb942464d3d3c
Ole Flaten <ole.flaten@inn.no>
2021-08-28 13:59:32 +0200

    Cleaned up VisualObject.
    Removed unneeded variables, includes and code.

--- visualobject.h
+++ visualobject.h
@@ -3,37 +3,17 @@
 
 #include <QOpenGLFunctions_4_1_Core>
 #include <vector>
-#include "vertex.h"
-#include "matrix4x4.h"
-#include "shader.h"
 #include "components.h"
-
-class RenderWindow;
 
 class VisualObject : public QOpenGLFunctions_4_1_Core {
 public:
     VisualObject();
     virtual ~VisualObject();
-    virtual void init();
-    virtual void draw()=0;
-
-    gsl::Matrix4x4 mMatrix;
+    virtual void init() = 0;
+    virtual void draw() = 0;
 
     std::string mName;
-
-    RenderWindow *mRenderWindow{nullptr}; //Just to be able to call checkForGLerrors()
-
     std::vector<Components*> mComponents;
-
-protected:
-    std::vector<Vertex> mVertices;
-    std::vector<GLuint> mIndices;
-
-    GLuint mVAO{0};
-    GLuint mVBO{0};
-    GLuint mEAB{0}; //holds the indices (Element Array Buffer - EAB)
-
-
 };
 #endif // VISUALOBJECT_H
 
