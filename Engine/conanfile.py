2681ca6c74f8e8950daace1aa482c626b5798275
Isak Wahl <56886173+isak90991515@users.noreply.github.com>
2021-09-03 22:03:24 +0200

    Some changes to cmakelist and conanfile.py

--- Engine/conanfile.py
+++ Engine/conanfile.py
@@ -1,6 +1,5 @@
 from conans import ConanFile, CMake
 
-
 class ConanPackage(ConanFile):
 	name = "GameInJin"
 	version = "0.1"
@@ -10,15 +9,37 @@ class ConanPackage(ConanFile):
         ('glfw/3.3.4')
     ]
 	generators = "cmake"
-	default_options = {"glad:egl_version": None, "glad:gl_profile": "compatibility",
+	options = {
+		"shared": [True, False]
+	}
+	default_options = {
+		"shared": False,
+		"glad:egl_version": None, "glad:gl_profile": "compatibility",
 		"glad:gl_version": 4.6, "glad:gles1_version": None, "glad:extensions":'', "glad:gles2_version": None, "glad:glsc2_version": None, "glad:glx_version": None,
-		"glad:no_loader": False, "glad:shared": True, "glad:spec": "gl", "glad:wgl_version": None, "glfw:vulkan_static": False}
+		"glad:no_loader": False, "glad:shared": True, "glad:spec": "gl", "glad:wgl_version": None, "glfw:vulkan_static": False, "glfw:shared": True
+	}
+	exports_sources = "Source/*", "CMakeLists.txt"
+
+	def configure_cmake(self):
+		cmake = CMake(self)
+		cmake.configure()
+		return cmake
 
 	def imports(self):
 		self.copy("*.dll", dst="bin", src="bin")
-		self.copy("*.dylib", dst="bin", src="lib")
+		self.copy("*.dylib*", dst="bin", src="lib")
 
 	def build(self):
-		cmake = CMake(self)
-		cmake.configure()
+		cmake = self.configure_cmake()
 		cmake.build()
+		#cmake.test()
+
+	def package(self):
+		cmake = self.configure_cmake()
+		cmake.install()
+
+	def package_info(self):
+		self.cpp_info.libs = ["GameInJin"]
+
+    	
+    	
