555eb996b50ca87640317c10efe65b002dd9a16f
pmHaugen <60093440+pmHaugen@users.noreply.github.com>
2021-09-04 11:53:35 +0200

    Update components.h

--- components.h
+++ components.h
@@ -1,11 +1,23 @@
 #ifndef COMPONENTS_H
 #define COMPONENTS_H
 
+#include "constants.h"
+#include "matrix4x4.h"
+#include "vertex.h"
 
-class components
+
+class TransformComponents
 {
 public:
-    components();
+    TransformComponents();
+    gsl:Matrix4x4 mMatrix;
+};
+
+class MeshComponent
+{
+public:
+    std::vector<Vertex> mVertices;
+    std::vector<
 };
 
 #endif // COMPONENTS_H
