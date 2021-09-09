6dc6d8ecb324e896766e1255ccbccbe0270d8c96
Ole Flaten <ole.flaten@inn.no>
2021-09-09 09:49:02 +0200

    Added start of resource manager.
    Not working yet.

---
+++ resourcemanager.h
@@ -0,0 +1,16 @@
+#ifndef RESOURCEMANAGER_H
+#define RESOURCEMANAGER_H
+
+#include <string>
+#include <map>
+
+class ResourceManager
+{
+public:
+    ResourceManager();
+
+private:
+    void readFile(std::string filename);
+};
+
+#endif // RESOURCEMANAGER_H
