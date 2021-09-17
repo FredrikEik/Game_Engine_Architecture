84dcdcec4e278983fa443b1d9d0c3e40201ed3a8
pmHaugen <60093440+pmHaugen@users.noreply.github.com>
2021-09-17 12:00:05 +0200

    clean up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-#include <chrono>   //for sleep_for
-#include <thread>   //for sleep_for
 int main(int argc, char *argv[])
 {
     //Forces the usage of desktop OpenGL
