90cdef7d660547d404479612f8a7dd925ca1ec33
Rayvex <odin.giske@gmail.com>
2021-09-17 10:34:40 +0200

    forgot to setToIdentity
    TransformComp->mMatrix.setToIdentity();

--- cube.cpp
+++ cube.cpp
@@ -55,6 +55,8 @@ Cube::Cube()
     MeshComp->mVertices.push_back(Vertex{ 0.5f,  0.5f, -0.5f,    0.7f, 0.0f, 0.3f,    1.0f, 1.0f});
     MeshComp->mVertices.push_back(Vertex{-0.5f,  0.5f, -0.5f,    0.5f, 0.2f, 0.6f,    0.0f, 1.0f});
     MeshComp->mVertices.push_back(Vertex{ 0.5f,  0.5f,  0.5f,    0.5f, 0.2f, 0.6f,    1.0f, 0.0f});
+
+    TransformComp->mMatrix.setToIdentity();
 }
 
 Cube::~Cube() {}
