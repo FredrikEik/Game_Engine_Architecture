f94218efe6f3ffa53fb3bcd1ec092651e6214fc6
JonasBjorhei <60387147+JonasBjorhei@users.noreply.github.com>
2021-10-04 21:34:15 +0200

    Implementing code for the widget

--- detailswidget.cpp
+++ detailswidget.cpp
@@ -1,14 +1,105 @@
 #include "detailswidget.h"
 #include "ui_detailswidget.h"
 
-DetailsWidget::DetailsWidget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::DetailsWidget)
+#include "gameobject.h"
+#include "matrix4x4.h"
+#include "renderwindow.h"
+#include "components.h"
+
+DetailsWidget::DetailsWidget(QWidget *parent, float positionPace, float rotationPace, float scalePace) :
+    QWidget(parent), ui(new Ui::DetailsWidget), mPositionPace{positionPace}, mRotationPace{rotationPace}, mScalePace{scalePace}
 {
     ui->setupUi(this);
+    setFocusPolicy(Qt::NoFocus);
 }
 
 DetailsWidget::~DetailsWidget()
 {
     delete ui;
 }
+
+void DetailsWidget::init(Renderwindow *renderWindow, int index)
+{
+    mRenderwindow = renderWindow;
+    inSceneArrayIndex =index;
+    readPosition();
+    readRotation();
+    readScale();
+    clearFocus();
+}
+
+void DetailsWidget::readPosition()
+{
+
+}
+
+void DetailsWidget::readRotation()
+{
+
+}
+
+void DetailsWidget::readScale()
+{
+
+}
+
+void DetailsWidget::setPosition()
+{
+
+}
+
+void DetailsWidget::on_DoubleSpinBoxXPosition_valueChanged(double arg1)
+{
+
+}
+
+
+void DetailsWidget::on_DoubleSpinBoxYPosition_valueChanged(double arg1)
+{
+
+}
+
+
+void DetailsWidget::on_DoubleSpinBoxZPosition_valueChanged(double arg1)
+{
+
+}
+
+
+void DetailsWidget::on_DoubleSpinBoxXRotation_valueChanged(double arg1)
+{
+
+}
+
+
+void DetailsWidget::on_DoubleSpinBoxYRotation_valueChanged(double arg1)
+{
+
+}
+
+
+void DetailsWidget::on_DoubleSpinBoxZRotation_valueChanged(double arg1)
+{
+
+}
+
+
+void DetailsWidget::on_DoubleSpinBoxXScale_valueChanged(double arg1)
+{
+
+}
+
+
+void DetailsWidget::on_DoubleSpinBoxYScale_valueChanged(double arg1)
+{
+
+}
+
+
+void DetailsWidget::on_DoubleSpinBoxZScale_valueChanged(double arg1)
+{
+
+}
+
+
+
--- detailswidget.h
+++ detailswidget.h
@@ -2,6 +2,10 @@
 #define DETAILSWIDGET_H
 
 #include <QWidget>
+#include "vector3d.h"
+
+
+class Renderwindow;
 
 namespace Ui {
 class DetailsWidget;
@@ -12,11 +16,48 @@ class DetailsWidget : public QWidget
     Q_OBJECT
 
 public:
-    explicit DetailsWidget(QWidget *parent = nullptr);
+    explicit DetailsWidget(QWidget *parent = nullptr, float positionPace = 1.f, float rotationPace = 1.f, float scalePace = 1.f);
     ~DetailsWidget();
+
+    void init(Renderwindow* renderWindow, int index);
+
+
+
+private slots:
+    void on_DoubleSpinBoxXPosition_valueChanged(double arg1);
+
+    void on_DoubleSpinBoxYPosition_valueChanged(double arg1);
+
+    void on_DoubleSpinBoxZPosition_valueChanged(double arg1);
+
+    void on_DoubleSpinBoxXRotation_valueChanged(double arg1);
+
+    void on_DoubleSpinBoxYRotation_valueChanged(double arg1);
+
+    void on_DoubleSpinBoxZRotation_valueChanged(double arg1);
+
+    void on_DoubleSpinBoxXScale_valueChanged(double arg1);
+
+    void on_DoubleSpinBoxYScale_valueChanged(double arg1);
+
+    void on_DoubleSpinBoxZScale_valueChanged(double arg1);
 
 private:
     Ui::DetailsWidget *ui;
+
+    void readPosition();
+    void readRotation();
+    void readScale();
+    void setPosition();
+
+    float mPositionPace{1.f};
+    float mRotationPace{1.f};
+    float mScalePace {0.1};
+
+    int inSceneArrayIndex{-1};
+    Renderwindow* mRenderwindow {nullptr};
+
+
 };
 
 #endif // DETAILSWIDGET_H
