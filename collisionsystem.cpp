815ba4262dc6914693f10d465d76c033cb4df020
Pr0g1 <60387249+Pr0g1@users.noreply.github.com>
2021-10-06 10:38:34 +0200

    Forberedelser

--- collisionsystem.cpp
+++ collisionsystem.cpp
@@ -4,3 +4,13 @@ CollisionSystem::CollisionSystem()
 {
 
 }
+
+bool CollisionSystem::collisionTestPvsS()
+{
+    // s = c - p; if abs(s) <= r .. return true;
+}
+
+bool CollisionSystem::collisionTestSvsS()
+{
+    // s = c1 - c2; if (abs(s)^2) <= (r1+r2)^2 .. return true;
+}
