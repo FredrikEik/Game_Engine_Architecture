b654546e42d8a66a4c0a69d23b1409bd4027b4b5
tobiasnes <60387446+tobiasnes@users.noreply.github.com>
2021-10-31 15:36:26 +0100

    Spawner funker helt, spawner blokker å flytter dem riktig

--- spawner.cpp
+++ spawner.cpp
@@ -10,11 +10,16 @@ Spawner::Spawner(ObjectFactory* ObjFactory)
 void Spawner::SpawnRow()
 {
     ObjSpawnFactory->createObject("Cube");
+    SpawnedObject = ObjSpawnFactory->mGameObject.back();
     if(SpawnedObject)
         SpawnedObject->getTransformComp()->mMatrix.translateX(0.0f);
-    //ObjSpawnFactory->createObject("Cube");
-    //SpawnedObject->getTransformComp()->mMatrix.translateX(1.0f);
-    //ObjSpawnFactory->createObject("Goat");
-    //SpawnedObject->getTransformComp()->mMatrix.translateX(2.0f);
+    ObjSpawnFactory->createObject("Cube");
+    SpawnedObject = ObjSpawnFactory->mGameObject.back();
+    if(SpawnedObject)
+        SpawnedObject->getTransformComp()->mMatrix.translateX(1.0f);
+    ObjSpawnFactory->createObject("Cube");
+    SpawnedObject = ObjSpawnFactory->mGameObject.back();
+    if(SpawnedObject)
+        SpawnedObject->getTransformComp()->mMatrix.translateX(2.0f);
 
 }
