52f3afa63ee007b06802711f6a1c02f566ce05c3
FredrikEik <fes221099@hotmail.no>
2021-10-31 21:51:04 +0100

    midlertidlig transform funskjon

--- transformwidget.cpp
+++ transformwidget.cpp
@@ -1,5 +1,7 @@
 #include "transformwidget.h"
 #include "ui_transformwidget.h"
+#include "rendersystem.h"
+#include "coreengine.h"
 
 transformWidget::transformWidget(QWidget *parent) :
     QWidget(parent),
@@ -11,4 +13,39 @@ transformWidget::transformWidget(QWidget *parent) :
 transformWidget::~transformWidget()
 {
     delete ui;
+}
+
+void transformWidget::on_rotX_valueChanged(double arg1)
+{
+
+   ui->rotX->setValue(arg1);
+   //tydligvis er y x i dette tilfelle..
+   CoreEngine::getInstance()->player->mTransform->mMatrix.rotateY(arg1);
+
+}
+
+void transformWidget::on_rotY_valueChanged(double arg1)
+{
+
+}
+
+void transformWidget::on_rotZ_valueChanged(double arg1)
+{
+
+}
+
+//transform x
+void transformWidget::on_scaleX_valueChanged(double arg1)
+{
+    CoreEngine::getInstance()->player->mTransform->mMatrix.translateX(arg1);
+}
+//transform y
+void transformWidget::on_scaleY_valueChanged(double arg1)
+{
+
+}
+//transform z
+void transformWidget::on_scaleZ_valueChanged(double arg1)
+{
+
 }
--- transformwidget.h
+++ transformwidget.h
@@ -2,7 +2,7 @@
 #define TRANSFORMWIDGET_H
 
 #include <QWidget>
-
+#include "rendersystem.h"
 namespace Ui {
 class transformWidget;
 }
@@ -15,8 +15,23 @@ public:
     explicit transformWidget(QWidget *parent = nullptr);
     ~transformWidget();
 
+private slots:
+    void on_rotX_valueChanged(double arg1);
+
+    void on_rotY_valueChanged(double arg1);
+
+    void on_rotZ_valueChanged(double arg1);
+
+    void on_scaleX_valueChanged(double arg1);
+
+    void on_scaleY_valueChanged(double arg1);
+
+    void on_scaleZ_valueChanged(double arg1);
+
 private:
     Ui::transformWidget *ui;
+
+    RenderSystem* mRenderSystem{nullptr};
 };
 
 #endif // TRANSFORMWIDGET_H
