be24494a592a1e8bcff18b02cf1dfa7bff475ec3
pmHaugen <60093440+pmHaugen@users.noreply.github.com>
2021-11-03 15:26:42 +0100

    Splash screen and freeplay

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include "mainwindow.h"
+#include <QPixmap>
 #include <QApplication>
+<<<<<<< HEAD
+<<<<<<< HEAD
+#include <Qt>
+#include <QSplashScreen>
+=======
+>>>>>>> parent of 33b8247 (test)
+=======
+>>>>>>> parent of 33b8247 (test)
 
 int main(int argc, char *argv[])
 {
@@ -9,10 +18,28 @@ int main(int argc, char *argv[])
 
     //Makes an Qt application
     QApplication a(argc, argv);
+<<<<<<< HEAD
+<<<<<<< HEAD
+    QPixmap pixmap("../GEA2021/splash.png");
+    QSplashScreen splash(pixmap);
+    splash.show();
+    splash.showMessage("Loading...");
+    splash.raise();
+    QCoreApplication::processEvents();
+=======
+>>>>>>> parent of 33b8247 (test)
+=======
+>>>>>>> parent of 33b8247 (test)
 
     //Makes the Qt MainWindow and shows it.
     MainWindow w;
+
+    w.move(10, 10);     //Moves the program at start. Nice if you have large screen and editor on one side.
     w.show();
+    splash.raise();
+    //splash.hide();
+    splash.finish(&w);
 
     return a.exec();
 }
+
