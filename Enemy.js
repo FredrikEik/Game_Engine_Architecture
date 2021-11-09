d7dc2c2f60af39e510b4f432560192b7efc30587
dreadleet <kris_k4o@hotmail.com>
2021-11-10 00:53:56 +0100

    script, no work
    cake is a lie...

--- Enemy.js
+++ Enemy.js
@@ -1,3 +1,8 @@
-function func() {
+var Name = "Enemy";
+var Health = 100.0;
+var Speed = 1.0;
+var Strength = 20;
 
+function takeDamage(str, hp) {
+    return hp - str;
 }
--- player.js
+++ player.js
@@ -1,3 +1,8 @@
-function func() {
+var Name = "Player 1";
+var Health = 100.0;
+var Speed = 1.5;
+var Strength = 100;
 
+function takeDamage(str, hp) {
+    return hp - str;
 }
