847511b4b02031ec3fa4e447716d7691299353c3
Johannes-s-b <johannesborgaas@hotmail.com>
2021-11-17 13:48:52 +0100

    Cleaned up the main loop a lot

--- Engine/Source/Engine/Engine.cpp
+++ Engine/Source/Engine/Engine.cpp
@@ -155,7 +155,6 @@ void Engine::loop()
 {
 	while (!glfwWindowShouldClose(window))
 	{
-
 		//// input
 		processInput(window);
 
@@ -164,135 +163,7 @@ void Engine::loop()
 		// can be used to calc deltatime
 		float currentFrame = glfwGetTime();
 
-		// feed inputs to dear imgui, start new frame
-		//ImGui_ImplOpenGL3_NewFrame();
-		//ImGui_ImplGlfw_NewFrame();
-		//ImGui::NewFrame();
-		////ImGui::SetWindowSize(ImVec2(400, 400));
-		////ImGui::SetNextWindowPos(ImVec2(150, 150));
-		//// render your GUI
-		////ImGui::DockSpace(0, ImVec2(800.0f, 600.f));
-		////ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
-		////auto Context = ImGui::CreateContext();
-		//
-		//ImGui::BeginGroup();
-		////ImGui::BeginChild("Demo window");
-		//ImGui::Begin("Demo window");
-		//if (ImGui::Button("Spawn cube"))
-		//{
-		//	uint32 entity = ECS->newEntity();
-		//	ECS->loadAsset(entity, DefaultAsset::CUBE);
-		//	ECS->addComponent<TransformComponent>(entity);
-		//	ECS->addComponent<AxisAlignedBoxComponent>(entity);
-		//	CollisionSystem::construct(entity, ECS);
-		//	std::cout << "Adding entity " << entity << '\n';
-	
-		//}
-		////// TEMP UPDATE
-		////ComponentManager<TransformComponent>* mng = ECS->getComponentManager<TransformComponent>();
-		////TransformSystem::moveAll(ECS->getComponentManager<TransformComponent>());
-
-		//if (ImGui::Button("Destroy entity 1"))
-		//{
-		//	ECS->destroyEntity(1);
-		//}
-		////ImGui::EndChild();
-		//ImGui::End();
-
-		//ImGui::Begin("TransformWidget");
-		////ImGui::BeginChild("TransformWidget");
-		//
-		//ImGui::InputInt("Entity", &EntityToTransform, 1, 10);
-		//glm::vec3 position = glm::vec3(ECS->getComponentManager<TransformComponent>()->getComponent(EntityToTransform).transform[3]);
-		//float test[3]{ position.x, position.y, position.z };
-		////ImGui::InputFloat3("Position", test);
-		//ImGui::DragFloat3("Position", &ECS->getComponentManager<TransformComponent>()->getComponent(EntityToTransform).transform[3].x, 0.1f, -10000.f, 10000.f);
-		////std::cout << "Setting position x: " << test[0] << " y: " << test[1] << " z: " << test[2]<<'\n';
-
-		////TransformSystem::setPosition(EntityToTransform, glm::vec3(test[0], test[1], test[2]), ECS);
-
-		//
-
-		////ImGui::EndChild();
-		//ImGui::End();
-		//ImGui::EndGroup();
-		//ImGui::DestroyContext(Context);
-
-		//{
-			//static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
-
-		//	// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-		//	// because it would be confusing to have two docking targets within each others.
-		//	ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-
-		//	ImGuiViewport* viewport = ImGui::GetMainViewport();
-		//	ImGui::SetNextWindowPos(viewport->Pos);
-		//	ImGui::SetNextWindowSize(viewport->Size);
-		//	//ImGui::SetNextWindowViewport(viewport->ID);
-		//	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-		//	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-		//	window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-		//	window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-
-
-		//	// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background and handle the pass-thru hole, so we ask Begin() to not render a background.
-		//	if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-		//		window_flags |= ImGuiWindowFlags_NoBackground;
-
-		//	// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
-		//	// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive, 
-		//	// all active windows docked into it will lose their parent and become undocked.
-		//	// We cannot preserve the docking relationship between an active window and an inactive docking, otherwise 
-		//	// any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
-		//	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-		//	ImGui::Begin("DockSpace", nullptr, window_flags);
-		//	ImGui::PopStyleVar();
-		//	ImGui::PopStyleVar(2);
-
-		//	// DockSpace
-		//	ImGuiIO& io = ImGui::GetIO();
-		//	if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
-		//	{
-		//		ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-		//		//ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-
-		//		static auto first_time = true;
-		//		if (first_time)
-		//		{
-		//			first_time = false;
-
-		//			//ImGui::DockBuilderRemoveNode(dockspace_id); // clear any previous layout
-		//			//ImGui::DockBuilderAddNode(dockspace_id, dockspace_flags | ImGuiDockNodeFlags_DockSpace);
-		//			//ImGui::DockBuilderSetNodeSize(dockspace_id, viewport->Size);
-
-		//			//// split the dockspace into 2 nodes -- DockBuilderSplitNode takes in the following args in the following order
-		//			////   window ID to split, direction, fraction (between 0 and 1), the final two setting let's us choose which id we want (which ever one we DON'T set as NULL, will be returned by the function)
-		//			////                                                              out_id_at_dir is the id of the node in the direction we specified earlier, out_id_at_opposite_dir is in the opposite direction
-		//			//auto dock_id_left = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Left, 0.2f, nullptr, &dockspace_id);
-		//			//auto dock_id_down = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Down, 0.25f, nullptr, &dockspace_id);
-
-		//			//// we now dock our windows into the docking node we made above
-		//			//ImGui::DockBuilderDockWindow("Down", dock_id_down);
-		//			//ImGui::DockBuilderDockWindow("Left", dock_id_left);
-		//			//ImGui::DockBuilderFinish(dockspace_id);
-		//		}
-		//	}
-
-		//	ImGui::End();
-
-		//	ImGui::Begin("Left");
-		//	ImGui::Text("Hello, left!");
-		//	ImGui::End();
-
-		//	ImGui::Begin("Down");
-		//	ImGui::Text("Hello, down!");
-		//	ImGui::End();
-		//}
-
 		CollisionBroadphaseDatastructure->update();
-
-
-		//CameraSystem::updateEditorCamera(editorCameraEntity, ECS, 0.016f);
 
 
 		//// RENDER
@@ -361,6 +232,7 @@ void Engine::loop()
 		else
 		{
 			//std::cout << "Editor camera'\n";
+			//TODO: Draw a game camera here
 			CameraSystem::updateEditorCamera(editorCameraEntity, ECS, 0.016f);
 		}
 
