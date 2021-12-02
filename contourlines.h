e20739eb4f0c414aa70ac0eab74e95edaf68271b
JonasBjorhei <60387147+JonasBjorhei@users.noreply.github.com>
2021-12-02 12:50:12 +0100

    ble heller ikke med

--- contourlines.h
+++ contourlines.h
@@ -1,11 +1,18 @@
 #ifndef CONTOURLINES_H
 #define CONTOURLINES_H
 
+#include "gameobject.h"
 
-class contourLines
+
+class contourLines : public GameObject
 {
 public:
     contourLines();
+    ~contourLines();
+
+    void draw() override;
+    void init() override;
+    void move(float x, float y, float z) override;
 };
 
 #endif // CONTOURLINES_H
