053978c1b83315328e68b0d909940f8ae7f23623
CrappyCracker <60387086+CrappyCracker@users.noreply.github.com>
2021-12-03 09:48:55 +0100

    Small Cleanup

--- rollingball.cpp
+++ rollingball.cpp
@@ -7,7 +7,7 @@
 
 RollingBall::RollingBall(int loadedMeshIndex)
 {
-    m_scale.scale(0.25,0.25,0.25);
+    m_scale.scale(1.f, 1.f, 1.f);
     meshIndex = loadedMeshIndex;
     m_modelMatrix.setToIdentity();
 }
@@ -40,12 +40,6 @@ bool RollingBall::findTriangle(unsigned int index, QVector3D &position, QVector3
     QVector3D p, q, r;
     std::vector<Vertex>& vertices = dynamic_cast<LasPlane*>(las_plane)->get_vertices();
 
-
-//    qDebug()<<"SIZE: "<<m_indices.size();
-//    p = vertices[m_indices[index]].getPosition();
-//    q = vertices[m_indices[index+1]].getPosition();
-//    r = vertices[m_indices[index+2]].getPosition();
-
     if((index+2)>=vertices.size())
         return false;
 
@@ -53,7 +47,6 @@ bool RollingBall::findTriangle(unsigned int index, QVector3D &position, QVector3
     q = vertices[index+1].getPosition();
     r = vertices[index+2].getPosition();
 
-    //outBaryCords = position.barycentricCoordinates(p, q, r);
     outBaryCords = gsm::MathFunctions::barycentricCoordinates(position.toVector2D(), p.toVector2D(), q.toVector2D(), r.toVector2D());
 
     if (outBaryCords.x() >= 0 && outBaryCords.y() >= 0 && outBaryCords.z() >= 0)
@@ -61,9 +54,7 @@ bool RollingBall::findTriangle(unsigned int index, QVector3D &position, QVector3
         outP = vertices[index].getPosition();
         outQ = vertices[index+1].getPosition();
         outR = vertices[index+2].getPosition();
-//        outP = vertices[m_indices[index]].getPosition();
-//        outQ = vertices[m_indices[index+1]].getPosition();
-//        outR = vertices[m_indices[index+2]].getPosition();
+
         return true;
     }
 
@@ -73,7 +64,6 @@ bool RollingBall::findTriangle(unsigned int index, QVector3D &position, QVector3
 float RollingBall::getHeight(QVector3D& positon)
 {
     QVector3D BaryCords, p, q, r;
-    //std::vector<Vertex>& vertices = dynamic_cast<TriangleSurface*>(triangle_surface)->get_vertices();
     std::vector<Vertex>& vertices = dynamic_cast<LasPlane*>(las_plane)->get_vertices();
 
     for (unsigned int i{}; i < vertices.size(); i += 3)
@@ -95,7 +85,6 @@ void RollingBall::calculateMovement(const QVector3D& p, const QVector3D& q, cons
     //Normal
     QVector3D pq = q - p;
     QVector3D pr = r - p;
-    //QVector3D normal = pq ^ pr;
     QVector3D normal = QVector3D::crossProduct(pq, pr);
     normal.normalize();
     current_normal = normal;
