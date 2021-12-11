3e8de1ad5847fae3424ad9e0847b5eb35d6754b9
Gaute Røise Samuelsen <gaute.samuelsen@gmail.com>
2021-12-11 15:05:24 +0100

    Doxygen

--- pch.cpp
+++ pch.cpp
@@ -3,9 +3,11 @@
 /** \mainpage Gautes spillmotor
 
    \section intro_sec Introduksjon
-   Dette er første seksjon
+   Spillmotor laget gjennom 5. semester (2021) i Spillmotorarkitektur-faget. Dette er en del av Spillteknologi og Simulering bacheleor-studie på høyskolen i innlandet. Dette er dokumentasjonen og forklaringen av strukturen.
 
-   \section install_sec Installation
-   Dette er andre seksjon
+
+   \section install_sec Kompilering
+   Skrevet i C++ med OpenGL for rendering. Kompatibel med MSVC2019 64bit kompilatoren. (Spesifikt brukt QT 6.1.3 - MSVC....)
+   Prosjektet bruker pre-compiled headers. OpenAL er satt som miljøvariabel på pc-en under navnet "OPENAL_HOME" som refererer til OpenAL installasjon 1.2.1-bin. Har du samme miljøvariabel skal DLL-fila også leses fra samme lokasjon.
 */
 
