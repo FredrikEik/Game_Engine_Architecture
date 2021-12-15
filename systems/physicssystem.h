70971a88d3d0c1d8efc19d121db095e0de1e5f43
Denizx11 <60387026+Denizx11@users.noreply.github.com>
2021-12-15 08:18:40 +0100

    Physics comments fixed

--- systems/physicssystem.h
+++ systems/physicssystem.h
@@ -1,7 +1,10 @@
 #ifndef PHYSICSSYSTEM_H
 #define PHYSICSSYSTEM_H
 #include "components.h"
-//! Struct containing Data that is used to simulate physics
+
+/**
+ * @brief The FloorData struct containing Data that is used to simulate physics.
+ */
 struct FloorData
 {
 
@@ -14,57 +17,104 @@ class PhysicsSystem
 {
 public:
     //! empty constructer
+    /**
+     * @brief PhysicsSystem Consturucter
+     */
     PhysicsSystem();
-    //!
-    //! \brief InitPhysicsSystem  - Here we set which surface we want to simulate physics
-    //! \param surfaceData - meshComponent of floor
-    //! \param inVertexData - Vertex data, this can be anything, in case you need to simulate lower poly floor
-    //!
+    /**
+     * @brief InitPhysicsSystem   - Here we set which surface we want to simulate physics.
+     * @param surfaceData  - meshComponent of floor.
+     * @param inVertexData  - Vertex data, this can be anything, in case you need to simulate lower poly floor.
+     */
     void InitPhysicsSystem(MeshComponent* surfaceData, std::vector<Vertex> inVertexData);
-    //!
-    //! \brief move - Main physics function
-    //! \param deltaTime - Using delta time to calculate distances accuratley
-    //! \param Transf - Transform component of the object that is colliding with surface
-    //! \param radius - Radius of the object thats simulated
-    //!
+
+    /**
+     * @brief move  - Main physics function.
+     * @param deltaTime  - Using delta time to calculate distances accuratley.
+     * @param Transf - Transform component of the object that is colliding with surface.
+     * @param radius  - Radius of the object thats simulated.
+     */
     void move(float deltaTime, TransformComponent* Transf, float radius);
 
 
 
 private:
-    //! Struct member
+    /**
+     * @brief Data Struct member.
+     */
     FloorData Data;
-    //! Used to remove extra bouncec - might be legacy
+    /**
+     * @brief once Used to remove extra bouncec - might be legacy.
+     */
     bool once = true;
-    //! bool to check if you are on a triangle
+    /**
+     * @brief onTriangle bool to check if you are on a triangle.
+     */
     bool onTriangle = false;
-    //! Copy of the surface meshComponent
+    /**
+     * @brief mSurfaceData Copy of the surface meshComponent.
+     */
     MeshComponent* mSurfaceData = new MeshComponent();
-    //! copy of surface vertexData
+    /**
+     * @brief vertexData   copy of surface vertexData.
+     */
     std::vector<Vertex> vertexData;
-    //! Main barysentric function
-    //! here we get FloorData - if we find triangle
+
+    /**
+     * @brief FindTriangle Main barysentric function. here we get FloorData - if we find triangle.
+     * @param Transf - Transformcomponent of object we want to check.
+     */
     void FindTriangle(TransformComponent* Transf);
-    //! Normal calculation
-    //! @return normalized normal vector
+
+    /**
+     * @brief CalcPlaneNormal Normal calculation.
+     * @param p1 Vertex 1.
+     * @param p2 Vertex 2.
+     * @param p3 Vertex 3.
+     * @return normalized normal vector.
+     */
     QVector3D CalcPlaneNormal(QVector3D p1,QVector3D p2,QVector3D p3);
-    //! Barysentric calculation
-    //! @return barysentric Vector , if numbers are positive and between 0 and 1, then we are on correct triangle
+
+    /**
+     * @brief Barysentric Barysentric calculation.
+     * @param p1 Vertex 1.
+     * @param p2 Vertex 2.
+     * @param p3 Vertex 3.
+     * @param pos position of physics object.
+     * @return barysentric Vector , if numbers are positive and between 0 and 1, then we are on correct triangle.
+     */
     QVector3D Barysentric(QVector3D p1,QVector3D p2,QVector3D p3, QVector3D pos);
-    //! converter from QVector3D to gsl::Vector3D
+    //!
+    /**
+     * @brief MakeGSLvec3D converter from QVector3D to gsl::Vector3D.
+     */
     gsl::Vector3D MakeGSLvec3D(QVector3D vec);
-    //! Converter from gsl::Vector3D to QVector3D
-    //! gsl::Vector3D misses some important functionality
+
+    /** gsl::Vector3D misses some important functionality
+     * @brief MakeQvec3D Converter from gsl::Vector3D to QVector3D.
+     * @param vec
+     * @return
+     */
     QVector3D MakeQvec3D(gsl::Vector3D vec);
-    //! mirror function
-    //! @param normal is the surface normal
-    //! @param Vector is velocity vector of physics simulated object
-    //! @return Velocity vector mirrored around the surface normal
+
+    /**
+     * @brief MirrorVector mirror function.
+     * @param Vector velocity vector of physics simulated object.
+     * @param normal  is the surface normal.
+     * @return vector mirrored around the surface normal.
+     */
     QVector3D MirrorVector(QVector3D Vector, QVector3D normal);
-    //! @brief DistanceTraveled - physics calculation of distance based on time
-    //!@param Velocity - velocity vector of object
-    //! @param Acceleration - Acc Vector of object
-    //! @param DT - Delta time
+    //! @brief DistanceTraveled
+    //!@param Velocity
+    //! @param Acceleration
+    //! @param DT
+    /**
+     * @brief DistanceTraveled - physics calculation of distance based on time.
+     * @param Velocity - velocity vector of object.
+     * @param Acceleration - Acc Vector of object.
+     * @param DT  - Delta time.
+     * @return Distance traveleld - this gets added to translate function later. :D
+     */
     QVector3D DistanceTraveled(QVector3D Velocity, QVector3D Acceleration, float DT);
 };
 
